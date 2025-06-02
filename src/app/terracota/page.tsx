47958f54074a6ac05573f242473cfd2cfdd5c9fd
rodCoder <hrodrigosilva@gmail.com>
2025-06-02 11:33:01 +0100

    removing use state

--- src/app/terracota/page.tsx
+++ src/app/terracota/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { useState } from 'react';
 import Image from 'next/image';
 import { MapPin, Phone, Mail } from 'lucide-react';
 import Navigation from '../components/Navigation/Navigation';
