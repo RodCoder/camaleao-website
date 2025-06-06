2c2f8d44be6026f0886c5f9eeada8a7b9278365d
rodCoder <hrodrigosilva@gmail.com>
2025-06-06 13:05:03 +0100

    removed ude effect

--- src/app/components/landing/Navigation/Navigation.tsx
+++ src/app/components/landing/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Navigation = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
