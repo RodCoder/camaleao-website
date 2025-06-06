a8e107087a6b08fd4e9822a96c998d7aedbc7f8e
rodCoder <hrodrigosilva@gmail.com>
2025-06-06 09:06:29 +0100

    fixed bug

--- src/app/terracota/page.tsx
+++ src/app/terracota/page.tsx
@@ -13,16 +13,6 @@ import ContactForm from '../components/landing/ContactForm/ContactForm';
 import Footer from '../components/landing/Footer/Footer';
 import { useState } from 'react';
 import Loader from '../components/Loader';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: "Camaleão - A dois passos do mar",
-  description: "Abrace a beleza da vida costeira com um empreendimento sustentável e bem planeado que proporciona luxo e paz de espírito. Perfeitas para viver, passar férias ou até mesmo para alugar turístico, estas propriedades exclusivas oferecem flexibilidade e conveniência em Castro Marim.",
-  keywords: "Camaleão, Castro Marim, empreendimento imobiliário, apartamentos, moradias, vista mar, Algarve, propriedades exclusivas, investimento imobiliário, resort, praia, sustentável, Portugal, turismo, férias, luxo",
-  openGraph: {
-    images: 'https://www.praiadocamaleao.pt/morad-01.png',
-  },
-};
 
 const Home = () => {
   const [showLoader, setShowLoader] = useState(true);
